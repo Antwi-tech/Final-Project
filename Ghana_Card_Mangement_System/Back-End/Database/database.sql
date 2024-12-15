7270c6cdb471f40b8618dff1ac7b5ef3175c3f8f
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-12-15 04:51:16 +0000

    created database for ghana card management system

---
+++ Ghana_Card_Mangement_System/Back-End/Database/database.sql
@@ -0,0 +1,9 @@
+-- Create a Simple database ghana card holds the data / schema of the card and person
+CREATE DATABASE IF NOT EXISTS Ghana_Card;
+
+
+SHOW DATABASES;
+USE Ghana_Card;
+SHOW TABLES;
+
+-- DROP DATABASE Ghana_Card;
